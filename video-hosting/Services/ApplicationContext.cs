667b05b319e55b8a96227d3616c5fead9ede2ed6
DOdGe <dimacernovolov@gmail.com>
2023-10-13 11:19:37 +0300

    Created a thumbnail display for added videos. Closed #3

---
+++ video-hosting/Services/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using video_hosting.Models;
+
+namespace video_hosting.Services
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Video> Videos => Set<Video>(); 
+        
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=database.db");
+        }
+    }
+}
---
+++ video-hosting/Services/VideoService.cs
@@ -0,0 +1,12 @@
+﻿using video_hosting.Models;
+
+namespace video_hosting.Services
+{
+    public class VideoService
+    {
+        public static List<Video> GetAllVideos(ApplicationContext database)
+        {
+            return database.Videos.ToList();
+        }
+    }
+}
